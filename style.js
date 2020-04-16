44a3c89fa6fcb1872b7d7301aa13d8c772109fc4
Carmen <carmenpleitez20@gmail.com>
2020-04-16 00:04:04 -0400

    ee

--- style.js
+++
@@ -1,7 +0,0 @@
-var clickMeButton = document.querySelector(".example");
-
-//eventlistener
-clickMeButton.addEventListener("click", function(){
-  
-  document.body.classList.toggle("darkmode");
-})
